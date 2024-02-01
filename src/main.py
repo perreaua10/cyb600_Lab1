d84a3407b5c5e9843d215a12ff7177912c286adf
perreaua10 <perreaua@my.canisius.edu>
2024-01-31 20:09:08 -0500

    finished web server part of the lab

--- src/main.py
+++ src/main.py
@@ -1,10 +1,21 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+from datetime import datetime
 
-app = FastAPI()
+app = FastAPI() #creating our 'app' here. this is what we call in our command to run
+
+#TO RUN: uvicorn main:app --reload
+#This must be from inside the SRC folder
 
 
-@app.get("/api/v1/hello")
+@app.get("/time", response_class=HTMLResponse)
 async def root():
-    return {"message": "This is RunLog, The Ultimate Running Log Experience"}
+    return """
+    <html>
+        <h1> """ + "Current Time is: " + str(datetime.now().strftime("%H:%M:%S")) + """
+        </h1>
+    </html>
+    """
+    
 
-#hosted simply at https://runlog-api.onrender.com/docs
+
